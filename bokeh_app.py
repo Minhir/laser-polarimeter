16a12a667d5b5bb96241cacd71d281be0c75cf49
Ivan Nikolaev <i.b.nikolaev@inp.nsk.su>
2018-06-22 06:19:18 +0700

    fix format error

--- bokeh_app.py
+++ bokeh_app.py
@@ -516,7 +516,7 @@ def app(doc, hist_storage_, data_storage_, freq_storage_, depolarizer, names):
                 #freq_error = abs(Depolarizer.speed*param['error'])
                 energy = depolarizer.frequency_to_energy(freq) if freq != 0 else 0
                 #energy_window.text = f"<p>Частота: {freq} +- {freq_error}, энергия: {energy}</p>"
-                energy_window.text = "<p>Частота: %8.1f +- %.1f, энергия: 7.3f +- %.1f</p>" % (freq, freq_error, energy, 0)
+                energy_window.text = "<p>Частота: %8.1f +- %.1f, энергия: %7.3f +- %.1f</p>" % (freq, freq_error, energy, 0)
 
         fit_handler["fit_line"] = asym_fig.line(fit_line_real_x_axis,
                                                 fit.get_line(name, fit_line_x_axis, [x['value'] for x in params_]),
